cc705d87983c1641a0f9a7c05cbeb29e2f3931ad
Bruno Fontes <brunoxdlols24@gmail.com>
2026-09-22 16:39:59 -0300

    Keep Sicredi addon jobs outside MK-AUTH cleanup path

--- jobs/SICREDIAPI/run_conciliar_sicredi_desconto.sh
+++ jobs/SICREDIAPI/run_conciliar_sicredi_desconto.sh
@@ -9,5 +9,5 @@ mkdir -p "$LOG_DIR"
 
 {
   echo "===== $(date '+%Y-%m-%d %H:%M:%S') ====="
-  /usr/bin/flock -n "$LOCK_FILE" /opt/php8/bin/php /opt/mk-auth/jobs/SICREDIAPI/conciliar_sicredi_desconto.php --days=15 --apply
+  /usr/bin/flock -n "$LOCK_FILE" /opt/php8/bin/php "${MKAUTH_DIR:-/opt/mk-auth}/jobs/SICREDIAPI_ADDON/conciliar_sicredi_desconto.php" --days=15 --apply
 } >> "$LOG_FILE" 2>&1
